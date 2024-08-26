48e026e29a9d29b15bf807b9ed021e82dceb0ff7
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-26 14:41:27 -0400

    added sliding window

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+# @leet start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        maxP = 0
+
+        for r in range(len(prices)):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(profit, maxP)
+            else:
+                l = r
+
+        return maxP
+# @leet end
