6be3d951eee092b25392621246cf0560c11ca7b6
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-25 20:49:08 -0400

    2 pointers codes

---
+++ 11.container-with-most-water.py
@@ -0,0 +1,17 @@
+# @leet start
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height)-1
+        maxArea = 0
+        while l < r:
+            area = (r-l) * min(height[l], height[r])
+            maxArea = max(area, maxArea)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxArea
+
+# @leet end
---
+++ 15.3sum.py
@@ -0,0 +1,28 @@
+# @leet start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i,a in enumerate(nums):
+            if i > 0 and nums[i-1] == a:
+                continue
+            
+            l = i + 1
+            r = len(nums)-1
+
+            while l < r:
+                temp = a + nums[l] + nums[r]
+                if temp < 0:
+                    l += 1
+                elif temp > 0:
+                    r -= 1
+                else:
+                    res.append([a,nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+
+        return res
+# @leet end
