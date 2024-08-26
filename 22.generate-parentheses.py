70afc76fb80f7a33fb8c0be10750297df8d1ae6e
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-08-25 21:02:41 -0400

    Stack Code

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,23 @@
+# @leet start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+        def backtrack(open, close):
+            if open == close == n:
+                res.append("".join(stack))
+
+            if open < close:
+                stack.append(")")
+                backtrack(open+1, close)
+                stack.pop()
+            
+            if close < n:
+                stack.append("(")
+                backtrack(open, close+1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res
+
+# @leet end
